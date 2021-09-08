aaff97f09ff5c46c0ed6040769e1b6f385f23e9f
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-08 16:55:46 -0300

    Creamos un archivo json con datos de clientes

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,81 @@
+import {Cliente} from './cliente';
+
+export const CLIENTES: Cliente[] = [
+{
+    id:1,
+    nombre: 'Pablo',
+    apellido:'Gallegos',
+    email:'pbl.gllgs@gmail.com',
+    createAt:'2017-12-11'},
+
+{
+  id: 2,
+  nombre: 'Isabelle',
+  apellido: 'Medina',
+  email: 'isabellemedina@eyeris.com',
+  createAt: '2017-07-11'
+},
+{
+  id: 3,
+  nombre: 'Angela',
+  apellido: 'Craig',
+  email: 'angelacraig@eyeris.com',
+  createAt: '2017-12-28'
+},
+{
+  id: 4,
+  nombre: 'Barton',
+  apellido: 'Kennedy',
+  email: 'bartonkennedy@eyeris.com',
+  createAt: '2019-03-09'
+},
+{
+  id: 5,
+  nombre: 'Shawna',
+  apellido: 'Mclaughlin',
+  email: 'shawnamclaughlin@eyeris.com',
+  createAt: '2020-08-23'
+},
+{
+  id: 6,
+  nombre: 'Marguerite',
+  apellido: 'Levy',
+  email: 'margueritelevy@eyeris.com',
+  createAt: '2018-02-24'
+},
+{
+  id: 7,
+  nombre: 'Ingram',
+  apellido: 'Mckinney',
+  email: 'ingrammckinney@eyeris.com',
+  createAt: '2019-10-04'
+},
+{
+  id: 8,
+  nombre: 'Charlotte',
+  apellido: 'Paul',
+  email: 'charlottepaul@eyeris.com',
+  createAt: '2021-01-27'
+},
+{
+  id: 9,
+  nombre: 'Schneider',
+  apellido: 'Cunningham',
+  email: 'schneidercunningham@eyeris.com',
+  createAt: '2016-12-22'
+},
+{
+  id: 10,
+  nombre: 'Hardin',
+  apellido: 'Blake',
+  email: 'hardinblake@eyeris.com',
+  createAt: '2014-09-20'
+},
+{
+  id: 11,
+  nombre: 'Hyde',
+  apellido: 'Cox',
+  email: 'hydecox@eyeris.com',
+  createAt: '2019-04-09'
+}
+];
