561388fd7a7798cba14c5e6a9d7af15a63876bd4
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-08 16:17:42 -0300

    Creamos la tabla de clientes y la agregamos a la pagina principal

--- src/app/clientes/cliente.ts
+++ src/app/clientes/cliente.ts
@@ -2,7 +2,15 @@ export class Cliente {
   id: number;
   nombre: string;
   apellido: string;
-  fecha: string;
+  createAt: string;
   email: string;
+
+  constructor(id:number, nombre:string, apellido:string, createAt:string, email:string) {
+      this.id = id;
+      this.nombre = nombre;
+      this.apellido=  apellido;
+      this.createAt= createAt;
+      this.email = email;
+    }
 
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Cliente} from './cliente';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[] = [
+    {id:1, nombre: 'Pablo', apellido:'Gallegos',email:'pbl.gllgs@gmail.com', createAt:'2017-12-11'}
+  ];
 
   constructor() { }
 
