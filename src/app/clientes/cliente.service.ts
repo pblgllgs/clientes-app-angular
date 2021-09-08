c06d25c5381f08de4e7bcab324a5dc97851c677b
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-08 20:46:07 -0300

    Creamos el observable para la cominicación asincrona

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {CLIENTES} from './clientes.json';
 import {Cliente} from './cliente';
+import {of,Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class ClienteService {
 
   constructor() { }
 
-  getClientes(): Cliente []{
-    return CLIENTES;}
+  getClientes(): Observable <Cliente []>{
+    return of(CLIENTES);}
 }
