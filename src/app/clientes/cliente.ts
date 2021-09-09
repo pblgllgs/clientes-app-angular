fc2123120b4bf15bebb6d8831faf703787c6c3e3
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-09 19:54:36 -0300

    Agregamos el smbolo ? para indicar que las propiedades son opcionales

--- src/app/clientes/cliente.ts
+++ src/app/clientes/cliente.ts
@@ -1,26 +1,8 @@
 export class Cliente {
-  id: number;
-  nombre: string;
-  apellido: string;
-  createAt: string;
-  email: string;
-
-  constructor(id:number, nombre:string, apellido:string, createAt:string, email:string) {
-      this.id = id;
-      this.nombre = nombre;
-      this.apellido=  apellido;
-      this.createAt= createAt;
-      this.email = email;
-    }
+  id?: number;
+  nombre?: string;
+  apellido?: string;
+  createAt?: string;
+  email?: string;
 
 }
-/*
-constructor(
-  public id:number,
-  public nombre:string,
-  public apellido:string,
-  public createAt:string,
-  public email:string) {
-}
-
-*/
