09e3a982098d572ef43b48e9e6776a6866d387d8
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-09 19:53:38 -0300

    Creamos el metodo create para gregar clientes

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 //import {CLIENTES} from './clientes.json';
 import {Cliente} from './cliente';
 import {/*of,*/Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {map} from 'rxjs/operators';
 
 @Injectable({
@@ -10,7 +10,8 @@ import {map} from 'rxjs/operators';
 })
 export class ClienteService {
 
-  private urlEndPoint: string = 'http://localhost:8080/api/clientes'
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  private httpHeaders = new HttpHeaders({'Content-Type':'application/json'})
 
   constructor(private http: HttpClient) { }
 
@@ -20,4 +21,11 @@ export class ClienteService {
       map(response => response as Cliente[])
     );
   }
+
+  create(cliente: Cliente) : Observable <Cliente>{
+
+    return this.http.post<Cliente>(this.urlEndPoint,cliente,{headers: this.httpHeaders})
+  }
+
+
 }
