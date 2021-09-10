56ad18fcdcd85994d44bb7592d3e1bb55bf68b3b
pbl.gllgs <pbl.gllgs@gmail.com>
2021-09-10 12:13:42 -0300

    Agregamos el metodo para carggar clientes al editar

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from './cliente';
 import {ClienteService} from './cliente.service';
-import {Router} from '@angular/router'
+import {Router,ActivatedRoute} from '@angular/router'
 import swal from 'sweetalert2'
 
 
@@ -15,9 +15,20 @@ export class FormComponent implements OnInit {
     titulo: string = "Crear Cliente"
 
   constructor(private clienteService: ClienteService,
-  private router:Router) { }
+  private router:Router,
+  private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.cargarCliente()
+  }
+
+  cargarCliente(): void{
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id']
+      if(id){
+        this.clienteService.getCliente(id).subscribe( (cliente) => this.cliente = cliente)
+      }
+    })
   }
 
   public create():void {
@@ -26,7 +37,7 @@ export class FormComponent implements OnInit {
        this.router.navigate(['/clientes'])
        swal('Cliente guardado',`Cliente ${cliente.nombre} creado con éxito`,'success')
      }
-    )
+   );
   }
 
 }
